c006964b4ff0b51ca3cd0cf620637fb75be8c029
varungopalkp <varun@qubicle.co.in>
2021-03-10 18:20:02 +0530

    initial

---
+++ app/containers/HomePage/index.js
@@ -0,0 +1,57 @@
+/**
+ *
+ * HomePage
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import makeSelectHomePage from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+
+export function HomePage() {
+  
+  return (
+    <div>
+      <Helmet>
+        <title>SpaceX</title>
+        <meta name="SpaceX" content="SpaceX Dashboard Challenge" />
+      </Helmet>
+    </div>
+  );
+}
+
+HomePage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  homePage: makeSelectHomePage(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+const withReducer = injectReducer({ key: 'homePage', reducer });
+const withSaga = injectSaga({ key: 'homePage', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(HomePage);
